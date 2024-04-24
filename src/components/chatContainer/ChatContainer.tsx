eafe0b9e7b23b75d5588a4f78daab961ac7ccf13
Vera Angerer <vera.angerer@redbull.com>
2024-04-24 21:43:58 +0200

    feat: load data from openapi

--- src/components/chatContainer/ChatContainer.tsx
+++ src/components/chatContainer/ChatContainer.tsx
@@ -1,23 +1,54 @@
-import {SubCategory} from "../searchByTrendingOverview/TrendingCategory.tsx";
-import {FC} from "react";
-import {MicroTrend} from "../../utils/types.ts";
-import {createPrompt} from "../../utils/create-prompt.ts";
-import {MessageContainer} from "./MessageContainer.tsx";
+import { SubCategory } from "../searchByTrendingOverview/TrendingCategory.tsx";
+import { FC, useEffect, useState } from "react";
+import { MicroTrend } from "../../utils/types.ts";
+import { createPrompt } from "../../utils/create-prompt.ts";
+import { MessageContainer } from "./MessageContainer.tsx";
+import OpenAI from "openai";
 
 interface ChatContainerProps {
   subcategory: SubCategory | null;
   microTrend: MicroTrend | null;
 }
 
-export const ChatContainer: FC<ChatContainerProps> = (({ subcategory, microTrend }) => {
-  const promptString = createPrompt({ topic: subcategory?.heading ?? '', microTrends: microTrend });
+const makeRequest = async (promptString: string) => {
+  const apiKey = 'TEST';
+  const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true });
+  console.log(promptString);
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { "role": "system", "content": promptString }
+    ],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+
+  return completion.choices
+}
+
+export const ChatContainer: FC<ChatContainerProps> = ({ subcategory, microTrend }) => {
+  const [response, setResponse] = useState<OpenAI.Chat.Completions.ChatCompletion.Choice[]>([]);
+  const [promptString, setPromptString] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const prompt = createPrompt({ topic: subcategory?.heading ?? '', microTrends: microTrend });
+      setPromptString(prompt);
+      const res = await makeRequest(prompt);
+      setResponse(res);
+    };
+
+    fetchData();
+  }, [subcategory, microTrend]);
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <div style={{ maxWidth: '60%', display: 'flex', gap: '8px', flexDirection: 'column' }}>
         <MessageContainer message={promptString} userType={'user'} />
-        <MessageContainer message={'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'} userType={'assistant'} />
+        {response.map((message) => (
+          <MessageContainer message={message.message.content ?? 'DEFAULT'} userType={'assistant'} />
+        ))}
       </div>
     </div>
   )
-})
+}
--- src/components/chatContainer/MessageContainer.tsx
+++ src/components/chatContainer/MessageContainer.tsx
@@ -1,14 +1,14 @@
-import {FC} from "react";
-import {AvatarContainer} from "./AvatarContainer.tsx";
-import {GravityText} from "@gravity/web-components-react";
-import {UserType} from "../../utils/types.ts";
+import { FC } from "react";
+import { AvatarContainer } from "./AvatarContainer.tsx";
+import { GravityText } from "@gravity/web-components-react";
+import { UserType } from "../../utils/types.ts";
 
 export interface MessageContainerProps {
   message: string;
   userType: UserType;
 }
 
-export const MessageContainer: FC<MessageContainerProps> = (({ message, userType}) => {
+export const MessageContainer: FC<MessageContainerProps> = (({ message, userType }) => {
   return (
     <div style={{
       display: 'flex',
@@ -18,15 +18,31 @@ export const MessageContainer: FC<MessageContainerProps> = (({ message, userType
       padding: '24px',
       borderRadius: '24px'
     }}>
-      <AvatarContainer userType={userType}/>
-      <div style={{display: 'flex', flexDirection: 'column', gap: '4px', alignItems: 'start', textAlign: 'start'}}>
+      <AvatarContainer userType={userType} />
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '4px', alignItems: 'start', textAlign: 'start' }}>
         <GravityText size={'small'} weight={'bold'}>
           {userType === 'user' ? 'You' : 'Dora'}
         </GravityText>
-        <GravityText color="dark-subtle" size={'small'}>
-          {message}
-        </GravityText>
+
+        <ul>
+          {processText(message)}
+        </ul>
+
       </div>
     </div>
   )
 })
+
+const processText = (text: string) => {
+  const items = text.split(/\d+\./).filter(item => item.trim() !== '');
+  const message = (x: string) => x.trim().replace('*', '').replace('$', '').replace('"', '').replace('*', '')
+
+  const unorderedList = items.map((item, index) => <li key={index}>
+    <GravityText color="dark-subtle" size={'small'}>
+      {message(item)}
+    </GravityText>
+
+  </li>);
+
+  return unorderedList
+}
