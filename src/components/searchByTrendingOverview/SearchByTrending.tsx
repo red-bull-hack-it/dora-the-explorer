5820efd831aed5a2e29931be06bd31c0605ea56b
Vera Angerer <vera.angerer@redbull.com>
2024-04-25 08:55:44 +0200

    feat: use mocked data for sports and travel

--- src/components/searchByTrendingOverview/SearchByTrending.tsx
+++ src/components/searchByTrendingOverview/SearchByTrending.tsx
@@ -1,5 +1,5 @@
-import {FC, SetStateAction} from "react";
-import {MOCKED_SUBCATEGORIES, SubCategory, TrendingCategory} from "./TrendingCategory.tsx";
+import { FC, SetStateAction } from "react";
+import { MOCKED_SUBCATEGORIES_SPORTS, MOCKED_SUBCATEGORIES_CULTURE, SubCategory, TrendingCategory } from "./TrendingCategory.tsx";
 
 export interface SearchByTrendingProps {
   setSubcategory: React.Dispatch<SetStateAction<SubCategory | null>>;
@@ -9,12 +9,12 @@ export interface SearchByTrendingProps {
 export const SearchByTrending: FC<SearchByTrendingProps> = (({ setSubcategory, setCategory }) => {
   return (
     <div style={{
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '24px',
-  }}>
-      <TrendingCategory categoryName={'Trending in Sports'} subCategories={MOCKED_SUBCATEGORIES} setSubcategory={setSubcategory} setCategory={setCategory}/>
-      <TrendingCategory categoryName={'Trending in Culture'} subCategories={MOCKED_SUBCATEGORIES} setSubcategory={setSubcategory} setCategory={setCategory} />
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '24px',
+    }}>
+      <TrendingCategory categoryName={'Trending in Sports'} subCategories={MOCKED_SUBCATEGORIES_SPORTS} setSubcategory={setSubcategory} setCategory={setCategory} />
+      <TrendingCategory categoryName={'Trending in Travel'} subCategories={MOCKED_SUBCATEGORIES_CULTURE} setSubcategory={setSubcategory} setCategory={setCategory} />
     </div>
   );
 })
--- src/components/searchByTrendingOverview/TrendingCategory.tsx
+++ src/components/searchByTrendingOverview/TrendingCategory.tsx
@@ -1,7 +1,9 @@
-import {FC} from "react";
-import {TrendingCategoryCard} from "./TrendingCategoryCard.tsx";
-import {GravityButton, GravityHeading} from "@gravity/web-components-react";
-import {SearchByTrendingProps} from "./SearchByTrending.tsx";
+import { FC } from "react";
+import { TrendingCategoryCard } from "./TrendingCategoryCard.tsx";
+import { GravityButton, GravityHeading } from "@gravity/web-components-react";
+import { SearchByTrendingProps } from "./SearchByTrending.tsx";
+import sportsJson from '../../factories/sports.json';
+import travelJson from '../../factories/travel.json';
 
 export interface SubCategory {
   heading: string;
@@ -14,19 +16,78 @@ export interface TrendingCategoryProps extends Pick<SearchByTrendingProps, 'setS
   categoryName: string;
 }
 
-export const MOCKED_SUBCATEGORIES: SubCategory[] = [
-  { heading: 'Bassdash', volume: '2.9K', growth: '+9500%'},
-  { heading: 'Padel Balls', volume: '2.9K', growth: '+9500%'},
-  { heading: 'Owala', volume: '2.9K', growth: '+9500%'},
+type SearchHistoryItem = {
+  value: number;
+  time: string;
+};
+
+type SearchHistory = {
+  last_15_years: SearchHistoryItem[];
+  growth: Growth;
+};
+
+type Growth = Record<string, number>;
+
+type Data = {
+  keyword: string;
+  absolute_volume: string;
+  search_history: SearchHistory;
+};
+
+
+
+const mapJson = (json: { total?: number; result: any; }) => json.result.map((sports: unknown) => {
+  const x = sports as unknown as Data
+  console.log(sports, x, 'SPORTS')
+  const history = x.search_history
+  console.log(history?.growth[120])
+  return {
+    heading: x.keyword ?? 'Padel courts',
+    volume: formatNumber(x.absolute_volume as unknown as number),
+    growth: x.search_history?.growth[120] ?? '100'
+  }
+})
+
+const sportsMocked = mapJson(sportsJson)
+const travelMocked = mapJson(travelJson)
+
+console.log(travelJson.result)
+
+function formatNumber(num: number): string {
+  const suffixes = ["", "k", "M", "B", "T"];
+  const suffixNum = Math.floor(("" + num).length / 3);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let shortValue: any = parseFloat((suffixNum !== 0 ? (num / Math.pow(1000, suffixNum)) : num).toPrecision(2));
+  if (shortValue % 1 !== 0) {
+    shortValue = shortValue.toFixed(1);
+  }
+  const x = shortValue + suffixes[suffixNum];
+  if (x.includes('NaN')) {
+    return '2.9K'
+  }
+  return x
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const MOCKED_SUBCATEGORIES_SPORTS: SubCategory[] = [
+  ...sportsMocked
+  // { heading: 'Bassdash', volume: '2.9K', growth: '+9500%' },
+  // { heading: 'Padel Balls', volume: '2.9K', growth: '+9500%' },
+  // { heading: 'Owala', volume: '2.9K', growth: '+9500%' },
+]
+export const MOCKED_SUBCATEGORIES_CULTURE: SubCategory[] = [
+  ...travelMocked
 ]
 
-export const TrendingCategory: FC<TrendingCategoryProps> = (({ categoryName, subCategories, setSubcategory, setCategory}) => {
+console.log(sportsJson)
+
+export const TrendingCategory: FC<TrendingCategoryProps> = (({ categoryName, subCategories, setSubcategory }) => {
   return (
     <div style={{ textAlign: 'start', display: 'flex', flexDirection: 'column', gap: '8px' }}>
       <GravityHeading size={'x-large'} weight={'bold'} style={{ paddingBottom: '24px' }}>{categoryName}</GravityHeading>
       <div style={{ display: 'flex', gap: '8px', flexDirection: 'row' }}>
         {subCategories.map(({ heading, volume, growth }, index) => {
-          return <TrendingCategoryCard heading={heading} volume={volume} growth={growth} setSubcategory={setSubcategory} setCategory={() => setCategory(categoryName)} src={index + 1}/>
+          return <TrendingCategoryCard heading={heading} volume={volume} growth={growth} setSubcategory={setSubcategory} setCategory={() => setCategory(categoryName)} src={index + 1} />
         })}
       </div>
       <div style={{ marginTop: '8px', alignSelf: 'center' }}>
